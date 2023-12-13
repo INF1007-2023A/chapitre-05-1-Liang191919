7d96e0186d857d34c0879e2d8c8fe1163bf97ad7
Liang <shenghege@gmail.com>
2023-12-12 19:52:04 -0500

    done

--- exercice.py
+++ exercice.py
@@ -1,34 +1,80 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
 from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    return 0
+    if number < 0:
+        number -= number*2
+    return int(number)
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
-
-    return [""]
+    liste = []
+    for i in prefixes:
+        liste.append(i+suffixe)
+    return liste
 
 
 def prime_integer_summation() -> int:
-    return 0
-
+    def is_prime(nombre):
+        if nombre == 1:
+            return False
+        for i in range(2,nombre):
+            if nombre % i == 0:
+                return False
+        return True
+    compteur = 0
+    nombre = 0
+    for i in range(1, 1000):
+        if is_prime(i):
+            nombre += i
+            compteur += 1
+            if compteur == 100:
+                return nombre
 
 def factorial(number: int) -> int:
-    return 0
+    nombre = 1
+    for i in range(1,number+1):
+        nombre *= i
+    return nombre
 
 
 def use_continue() -> None:
-    pass
+    for i in range(1,11):
+        if i == 5:
+            continue
+        print(i)
 
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+    def contient_25(liste):
+        for i in liste:
+            if i == 25:
+                return True
+        return False
+    def contient_50(liste):
+        for i in liste:
+            if i == 50:
+                return True
+        return False
+    liste = []
+    for i in range(len(groups)):
+        if 10 < len(groups[i]) or len(groups[i]) <= 3:
+            liste.append(False)
+        else:
+            for j in groups[i]:
+                groups[i].sort()
+                print(groups[i])
+                if groups[i][-1] > 70 and contient_50(groups[i])==True and contient_25(groups[i]) == False:
+                    liste.append(False)
+                    break
+                elif j < 18 and contient_25(groups[i]) == False:
+                    liste.append(False)
+                    break
+                else:
+                    liste.append(True)
+                    break
+    return liste
 
 
 def main() -> None:
